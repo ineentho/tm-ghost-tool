b95fee707fe98cd659a9cfc590d383e324ddfb05
Henrik Karlsson <hk.henrik@gmail.com>
2021-08-08 22:49:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using GBX.NET;
+using GBX.NET.Engines.Game;
+
+namespace tm_ghost_tool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach (var filePath in args)
+			{
+				Console.WriteLine($"Updating {filePath}");
+				try
+				{
+					ProcessFile(filePath);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+				}
+			}
+		}
+
+		private static void ProcessFile(string path)
+		{
+			var map = GameBox.ParseNode<CGameCtnChallenge>(path);
+			map.ChallengeParameters.RaceValidateGhost = null;
+			SaveMap(map, path);
+		}
+
+		private static void SaveMap(CGameCtnChallenge map, string sourcePath)
+		{
+			var outDirectory = Path.Join(Path.GetDirectoryName(sourcePath), "NoGhost");
+			var outPath = Path.Join(outDirectory, Path.GetFileName(sourcePath));
+
+			Directory.CreateDirectory(outDirectory);
+			map.Save(outPath);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+A command line tool to remove ghosts from map files.
+
+Usage: tm-ghost-tool <map-file>...
+
+Updated maps will be created in a NoGhost folder next to the source file
